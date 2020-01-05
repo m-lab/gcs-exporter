73b8323ac0ed488956058b4d2c65fd7b55d52822
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-01-05 12:25:15 -0500

    Simplify calculations of next aligned time

--- main.go
+++ main.go
@@ -33,23 +33,17 @@ var (
 	mainCtx, mainCancel = context.WithCancel(context.Background())
 )
 
-const secInDay = 24 * 60 * 60
-
-func yesterday(now time.Time, offset int) time.Time {
-	// Offset additional 24hrs in the past to align on "most recent complete day"
-	return now.Add(time.Duration(-offset-secInDay) * time.Second).Truncate(secInDay * time.Second)
-}
-
-// untilNextUpdate calculates the duration between current time and the next scheduled update.
-func untilNextUpdate(cur time.Time, nextOffsetSec int) time.Duration {
-	// Convert next and current times to an offset in seconds.
-	curOffsetSec := cur.Hour()*60*60 + cur.Minute()*60 + cur.Second()
-	if nextOffsetSec > curOffsetSec {
-		// The next time is still in the current day.
-		return time.Duration(nextOffsetSec-curOffsetSec) * time.Second
+func nextAlignedTime(now time.Time, period, offset time.Duration) (time.Time, time.Duration) {
+	// Align current time with given period and offset.
+	aligned := now.Truncate(period).Add(offset)
+	if now.After(aligned) {
+		// We've missed the time today, so calculate delay between now and next period.
+		wait := period - now.Sub(aligned)
+		next := aligned.Add(wait)
+		return next.Add(-period), wait
 	}
-	// We've missed the next time for today, so add nextOffset to the time remaining today.
-	return time.Duration(nextOffsetSec+(secInDay-curOffsetSec)) * time.Second
+	// The current time is after the aligned time.
+	return aligned.Add(-period), aligned.Sub(now)
 }
 
 func main() {
@@ -64,26 +58,28 @@ func main() {
 		buckets[s] = storagex.NewBucket(client.Bucket(s))
 	}
 
-	// Calculate static offset to calculate the latest complete day.
-	offset := collectTime.Hour*60*60 + collectTime.Minute*60 + collectTime.Second
+	// Calculate static time offset used before calculating the latest complete day.
+	offset := time.Duration(collectTime.Hour*60*60+collectTime.Minute*60+collectTime.Second) * time.Second
 
-	// Initialize the collector using data from "yesterday".
-	c := gcs.NewCollector(buckets, yesterday(time.Now().UTC(), offset))
+	next, _ := nextAlignedTime(time.Now().UTC(), 24*time.Hour, offset)
+	// Initialize the collector starting an additional day in the past.
+	c := gcs.NewCollector(buckets, next.Add(-24*time.Hour))
 	prometheus.MustRegister(c)
 
 	srv := prometheusx.MustServeMetrics()
 	defer srv.Close()
 
 	for {
-		delay := untilNextUpdate(time.Now().UTC(), offset)
-		log.Printf("Sleeping %s until next update at %s", delay, time.Now().UTC().Add(delay))
+		next, delay := nextAlignedTime(time.Now().UTC(), 24*time.Hour, offset)
+		log.Printf("Sleeping: %s until next update at %s", delay, next)
+
 		select {
 		case <-mainCtx.Done():
 			return
 		case <-time.After(delay):
 			// NOTE: ignore Update errors.
 			// NOTE: sqhould only update once a day.
-			c.Update(mainCtx, yesterday(time.Now().UTC(), offset))
+			c.Update(mainCtx, next)
 		}
 	}
 }
